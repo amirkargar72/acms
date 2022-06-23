b04f14b072a9213d1d4073b4fa5e0ccf13e74175
amirkargar72 <amir_kindboy@yahoo.com>
2022-06-23 16:31:01 +0430

    Header Model and Migration and controller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', "indexController@main");
 
 Auth::routes();
 
